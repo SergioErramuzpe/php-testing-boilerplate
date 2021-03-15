8d45f85c73979886a980ec11e127cd180d628bee
erramuzpe111448 <sergio.erramuzpe@gmail.com>
2021-03-15 17:11:28 +0000

    Creado el constructor de TennisGame con los argumentos playerScores y modificacion de getScore para que el test FifteenLove funcione

--- src/TennisGame.php
+++ src/TennisGame.php
@@ -7,11 +7,24 @@ namespace Deg540\PHPTestingBoilerplate;
 class TennisGame
 {
 
+    var $player1Score;
+    var $player2Score;
 
+
+    /**
+     * TennisGame constructor.
+     */
+    public function __construct($player1Score, $player2Score)
+    {
+        $this->player1Score = $player1Score;
+        $this->player2Score = $player2Score;
+    }
 
     public function getScore () : string {
         if ($this->player1Score == $this->player2Score)
             return "Love all";
+        else
+            return "Fifteen - Love";
     }
 
 
