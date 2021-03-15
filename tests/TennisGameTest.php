28a201027ba92232ed2563906cfafa7e5a10f47a
erramuzpe111448 <sergio.erramuzpe@gmail.com>
2021-03-15 16:58:28 +0000

    Creadas las clases de TennisGame y TennisGameTest

---
+++ tests/TennisGameTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Deg540\PHPTestingBoilerplate\Test;
+
+use Deg540\PHPTestingBoilerplate\TennisGame;
+use PHPUnit\Framework\TestCase;
+
+class TennisGameTest extends TestCase
+{
+
+
+
+}
